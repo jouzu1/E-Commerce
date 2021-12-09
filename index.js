29a905a810b6f55c978aa4fe712614a048fe9e90
jouzu1 <jouzu1@gmail.com>
2021-12-09 21:19:03 +0700

    baru bikin server dan cara menjalankannya

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Halo")
+const express = require('express');
+const app = express();                          //Untuk menjalankan server backend
+
+const mongoose = require('mongoose');           //Untuk menyambungkan app ke DB Cloud Atlas Mongo
+
+const connString = require('./secret_connect'); //Secret Connection String to DB, dont share this string!!
+
+
+app.listen(5000,()=>{
+    console.log('Backend server sedang berjalan')
+});
+
+mongoose.connect(connString.connectionString).then(()=>{
+    console.log("Koneksi ke DB Berhasil")
+}).catch((err)=>{
+    console.log(err);
+});
+
