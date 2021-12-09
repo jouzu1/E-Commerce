4707e44d077c0463fd290e33c88925f785c456ed
jouzu1 <jouzu1@gmail.com>
2021-12-09 21:25:41 +0700

    update, tapi cara manggil connection stringnya dengan .env

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const mongoose = require('mongoose');           //Untuk menyambungkan app ke DB 
 
 const connString = require('./secret_connect'); //Secret Connection String to DB, dont share this string!!
 
+const dotenv = require('dotenv');               //Ini juga sama, memanggil secret key dari file .env
+
+dotenv.config();                                //Line ini untuk bisa menggunakan/memanggil file .env dengan cara process.env.*namaVariabelnya*
+
 
 app.listen(5000,()=>{
     console.log('Backend server sedang berjalan')
 });
 
-mongoose.connect(connString.connectionString).then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Koneksi ke DB Berhasil")
 }).catch((err)=>{
     console.log(err);
