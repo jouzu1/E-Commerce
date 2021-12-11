db5fe222e4ff1d3e20715b1fbd4663bcc20afb4a
jouzu1 <jouzu1@gmail.com>
2021-12-11 15:44:40 +0700

    add new libs, fixing register service POST method

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,23 @@
 const router = require('express').Router();
-const User = require('../Models/user')
+const User = require('../Models/user');
+const bcrypt = require('bcryptjs');                                     //Memanggil cryptojs untuk meng-encrypt password ke DB
+const dotenv = require('dotenv');                                       //Memanggil library dotenv agar bisa memanggil .env file
+
 
 //REGISTER USER
 router.post("/register", async(req,res)=>{                              //Menggunakan method POST untuk menulis object ke body di postman
     const newUser = new User({
         username : req.body.username,
         email : req.body.email,
-        password : req.body.password,
+        password : bcrypt.hashSync(req.body.password,bcrypt.genSaltSync(10)).toString(),            //Line ini digunakan utk meng-encrypt password, memanggil file .env serta menampilkan password kedalam response json dalam bentuk string
     })
     try{
         const SaveUser = await newUser.save();                          //Variabel dengan memakai method save() node js untuk menyimpan object newUser ke MongoDB
-        res.send(200,"User Teregistrasi " + newUser).json(SaveUser);    //res.send dipakai untuk memberikan response berupa hasil object yang kita POST di postman
+        // res.send(200, SaveUser);                                        
+        res.status(200).send(SaveUser);                                 //res.send dipakai untuk memberikan response berupa hasil object yang kita POST di postman
     }catch(err){
-        res.send(500,"Username, email, dan password harus terisi").json(err);
+        // res.send(500,err);
+        res.status(500).send(err);
     }
 })
 
